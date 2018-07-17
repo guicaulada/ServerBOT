97ad13d5ad427111aed6816239962e80bac012ba
Guilherme Caulada (Sighmir) <guistoppa1995@gmail.com>
2018-07-17 18:43:07 -0300

    feat(services): add support for multiple ports, change paping for psping

--- src/modules/services.js
+++ src/modules/services.js
@@ -1,17 +1,25 @@
 __ServerBOT.updateServices = () => {
   __ServerBOT.config.services.forEach((service) => {
-    let [address, port] = service.address.split(':');
-    let ping = __spawn('paping', [address, '-p', port, '-c', 3]);
-    let output = '';
-    ping.stdout.on('data', (data) => {
-      output = output + data.toString('utf8');
-    });
-    ping.on('close', (data) => {
-      if (output.search('Failed = 0') > 0) {
-        service.status = true;
-      } else if (output.search('Connected = 0') > 0) {
-        service.status = false;
-      }
+    let success = false;
+    service.ports.forEach((port) => {
+      let address = service.address + ':' + port;
+      let ping = __spawn('psping', ['-n', 3, '-w', 0, '-i', 0, address]);
+      let output = '';
+      ping.stdout.on('data', (data) => {
+        output = output + data.toString('utf8');
+      });
+      ping.on('close', (data) => {
+        let s = 'Sent =';
+        let r = 'Received =';
+        let sent = Number(output[output.search(s) + s.length + 1]);
+        let received = Number(output[output.search(r) + r.length + 1]);
+        if (sent > 0 && received > 0) {
+          service.status = true;
+          success = true;
+        } else if (!success) {
+          service.status = false;
+        }
+      });
     });
   });
 };
@@ -20,4 +28,5 @@ let servicePoller = () => {
   __ServerBOT.updateServices();
   setTimeout(servicePoller, __ServerBOT.config.updateInterval);
 };
-servicePoller();
+
+setTimeout(servicePoller, __ServerBOT.config.updateInterval);
