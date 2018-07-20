0698091de06a42592c8c8feb37bc4929edbdfaa9
Guilherme Caulada (Sighmir) <guistoppa1995@gmail.com>
2018-07-19 22:54:17 -0500

    feat(config): adds default channel for bot messages

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,7 @@ const config = {
   updateInterval: 300000,
   dns: process.env.SERVERBOT_DNS,
   adminRoles: ['405546509770162177', '155180032405274624', '382410412504776705'],
+  channel: '369609469543579658',
 };
 
 module.exports = config;
